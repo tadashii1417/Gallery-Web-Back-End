31f670ff1f70d4cac8bc3f5d1b5de0b30570efde
Huy Nguyen <kqhuy.nguyen@gmail.com>
2019-12-07 16:26:55 +0700

    updated uploaded images

---
+++ api/image/get_uploaded_images_of_user.php
@@ -0,0 +1,40 @@
+<?php
+// required headers
+include_once '../../config/header.php';
+include_once '../../config/database.php';
+include_once '../../objects/user.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+// $data = json_decode(file_get_contents("php://input"));
+if (empty($_GET['user_id'])) {
+    http_response_code(400);
+    echo (json_encode(['message' => 'missing user_id']));
+    return;
+}
+
+try {
+    $query = 'SELECT * FROM images
+    WHERE (user_id = :user_id) AND status = 1';
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':user_id', $_GET['user_id']);
+    if ($stmt->execute()) {
+        $ret = array();
+        while ($row = $stmt->fetchObject()) {
+            $ret[] = $row;
+        }
+        echo json_encode(["images" => $ret]);
+    } else {
+        http_response_code(400);
+        echo json_encode([
+            "message" => "Can't fetch images.",
+        ]);
+    }
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode([
+        "message" => "Can't fetch images.",
+        "error" => $e->getMessage(),
+    ]);
+}
