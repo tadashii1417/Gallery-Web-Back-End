f630edd36d7a8514b16e4afb5f3c7454303eba3a
Huy Nguyen <kqhuy.nguyen@gmail.com>
2019-12-07 10:01:43 +0700

    POST /image/like.php

---
+++ api/image/like.php
@@ -0,0 +1,58 @@
+<?php
+#####################################################
+#Date: 21:00 3/12/2019
+#Author: Dang Bao
+#In:  jwt from client
+#Out: Return all image in database with their owner info.
+#####################################################
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// required to encode json web token
+include_once '../../config/core.php';
+include_once '../../libs/php-jwt-master/src/BeforeValidException.php';
+include_once '../../libs/php-jwt-master/src/ExpiredException.php';
+include_once '../../libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once '../../libs/php-jwt-master/src/JWT.php';
+
+use \Firebase\JWT\JWT;
+
+include_once '../../config/database.php';
+include_once '../../objects/user.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+$database = new Database();
+$db = $database->getConnection();
+
+$jwt = isset($data->jwt) ? $data->jwt : "";
+
+if ($jwt) {
+    try {
+        $decoded = JWT::decode($jwt, $key, ['HS256']);
+        $query = 'INSERT INTO likes VALUES (:user_id, :image_id);';
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':user_id', $decoded->data->id); // user_id
+        $stmt->bindParam(':image_id', $data->image_id);
+        if ($stmt->execute()) {
+            echo json_encode(["message" => "successful update"]);
+        } else {
+            http_response_code(400);
+            print_r($stmt->errorInfo());
+            echo json_encode(["message" => "can't complete operation"]);
+        }
+    } catch (Exception $e) {
+        http_response_code(401);
+        echo json_encode([
+            "message" => "Access denied.",
+            "error" => $e->getMessage(),
+        ]);
+    }
+} else {
+    http_response_code(401);
+    echo json_encode(["message" => "Access denied."]);
+}
