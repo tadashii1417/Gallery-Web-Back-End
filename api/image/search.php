2d8010b92bbefe36493a2b60c5139a1204be288b
Huy Nguyen <kqhuy.nguyen@gmail.com>
2019-12-07 14:37:58 +0700

    Search

---
+++ api/image/search.php
@@ -0,0 +1,37 @@
+<?php
+// required headers
+include_once '../../config/header.php';
+
+
+include_once '../../config/database.php';
+include_once '../../objects/user.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+
+try {
+    $keyword = $_GET['keyword'];
+    $keyword = htmlspecialchars(strip_tags($keyword));
+    $query = 'SELECT * FROM images WHERE status = 1 AND description LIKE "%' . $keyword . '%"';
+    $stmt = $db->prepare($query);
+    if ($stmt->execute()) {
+        $ret = array();
+        while ($row = $stmt->fetchObject()) {
+            $ret[] = $row;
+        }
+        echo json_encode(["images" => $ret]);
+    } else {
+        http_response_code(400);
+        echo json_encode([
+            "message" => "Can't fetch images.",
+            "error" => $stmt->errorInfo()
+        ]);
+    }
+} catch (Exception $e) {
+    http_response_code(401);
+    echo json_encode([
+        "message" => "Can't fetch images.",
+        "error" => $e->getMessage(),
+    ]);
+}
