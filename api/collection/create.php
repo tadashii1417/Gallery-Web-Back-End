66abafe6642528a98e8b07ac9b9fc4f27bdccf62
tadashii1417 <tadashii1417@gmail.com>
2019-12-11 23:56:56 +0700

    updated

--- api/collection/create.php
+++ api/collection/create.php
@@ -1,11 +1,6 @@
 <?php
 // required headers
-header("Access-Control-Allow-Origin: http://localhost/web/backend/");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+include_once '../../config/header.php';
 // required to encode json web token
 include_once '../../config/core.php';
 include_once '../../libs/php-jwt-master/src/BeforeValidException.php';
@@ -31,7 +26,7 @@ $jwt = $data->jwt;
 
 try {
     $decoded = JWT::decode($jwt, $key, ['HS256']);
-} catch(Exception $e) {
+} catch (Exception $e) {
     http_response_code(403);
     echo json_encode(array("message" => "Access denied."));
 }
@@ -44,9 +39,8 @@ if (!isset($data->name)) {
 
 $collection->name = $data->name;
 $collection->description = isset($data->description) ? $data->description : "";
-$collection->user_id= $decoded->data->id;
+$collection->user_id = $decoded->data->id;
 // TODO: check if username exists.
-
 
 // create the user
 if ($collection->create()) {
